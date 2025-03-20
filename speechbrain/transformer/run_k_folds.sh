80195c81f903e22f424e7255bfdc77df7fa7921a
flatala <franio.latala@gmail.com>
2025-03-20 12:56:34 +0100

    updates

--- speechbrain/transformer/run_k_folds.sh
+++ speechbrain/transformer/run_k_folds.sh
@@ -1,5 +1,14 @@
-python train_k_folds.py hparams/conformer_small_fold_1.yaml
-python train_k_folds.py hparams/conformer_small_fold_2.yaml
-python train_k_folds.py hparams/conformer_small_fold_3.yaml
-python train_k_folds.py hparams/conformer_small_fold_4.yaml
-python train_k_folds.py hparams/conformer_small_fold_5.yaml
+echo 'running split 1'
+python train.py hparams/conformer_small_fold_1.yaml
+
+# echo 'running split 2'
+# python train.py hparams/conformer_small_fold_2.yaml
+
+# echo 'running split 3'
+# python train.py hparams/conformer_small_fold_3.yaml
+
+# echo 'running split 4'
+# python train.py hparams/conformer_small_fold_4.yaml
+
+# echo 'running split 5'
+# python train.py hparams/conformer_small_fold_5.yaml

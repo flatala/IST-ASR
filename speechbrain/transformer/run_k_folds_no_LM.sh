e9209f8c18a5c60ef29cf8575deea8bda10bdcb1
a-sasin <amelia.sasin@gmail.com>
2025-03-20 14:30:11 +0100

    run no lm

---
+++ speechbrain/transformer/run_k_folds_no_LM.sh
@@ -0,0 +1,5 @@
+python train.py hparams/no_lm/conformer_small_fold_1.yaml
+python train.py hparams/no_lm/conformer_small_fold_2.yaml
+python train.py hparams/no_lm/conformer_small_fold_3.yaml
+python train.py hparams/no_lm/conformer_small_fold_4.yaml
+python train.py hparams/no_lm/conformer_small_fold_5.yaml

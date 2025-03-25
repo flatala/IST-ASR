728e167067934e0a847996077468ef42564b9d80
a-sasin <amelia.sasin@gmail.com>
2025-03-25 11:55:57 +0100

    added weights for 060

---
+++ speechbrain/transformer/run_k_folds_060lm.sh
@@ -0,0 +1,14 @@
+# echo 'running split 1'
+# python train.py hparams/no_lm/conformer_small_fold_1.yaml
+
+# echo 'running split 2'
+# python train.py hparams/no_lm/conformer_small_fold_2.yaml
+
+# echo 'running split 3'
+# python train.py hparams/no_lm/conformer_small_fold_3.yaml
+
+echo 'running split 4'
+python train.py hparams/no_lm/conformer_small_fold_4.yaml
+
+echo 'running split 5'
+python train.py hparams/no_lm/conformer_small_fold_5.yaml

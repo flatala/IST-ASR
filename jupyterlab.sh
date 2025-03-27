adbcc286d5944aba1fce4a9903af54d088e44f32
flatala <franio.latala@gmail.com>
2025-03-27 22:23:27 +0100

    add nemo files to lfs

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=jupyterlab
-#SBATCH --partition=gpu-a100
-#SBATCH --time=04:00:00
+#SBATCH --partition=gpu-a100-small
+#SBATCH --time=03:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=1
@@ -12,19 +12,13 @@
 # Load modules:
 module load 2023r1
 module load miniconda3
-# module load cuda/11.6
-# module load openmpi/4.1.4
-# module load py-torch/1.12.1
-# module load py-pip
-# module load py-numpy
-# module load py-pyyaml
-# module load py-tqdm
-# module load ffmpeg
+module load ffmpeg
+module load cuda/12.1
 
 unset CONDA_SHLVL
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
-conda activate IST-ASR
+conda activate nemo
 cat /etc/hosts
 jupyter lab --ip=0.0.0.0 --port=8888
 conda deactivate
